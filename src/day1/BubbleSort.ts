470037d042486d58bcfb9d9cd7394a14a2402f2f
Kadeem Jackson <kdjackson35@gmail.com>
2024-09-17 15:40:22 -0400

    Add BubbleSort with both loops and recursion

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,35 @@
-export default function bubble_sort(arr: number[]): void {
+// export default function bubble_sort(arr: number[]): void {
+//     for (let i = 0; i < arr.length; i++) {
+//         for (let j = 0; j < arr.length - 1 - i; j++) {
+//             if (arr[j] > arr[j + 1]) {
+//                 const tempNum = arr[j];
+//                 arr[j] = arr[j + 1];
+//                 arr[j + 1] = tempNum;
+//             }
+//         }
+//     }
+// }
 
+// Recursive Solution
+export default function bubble_sort(
+    arr: number[],
+    n: number = arr.length,
+): void {
+    if (n === 1) {
+        return;
+    }
+
+    for (let i = 0; i < n - 1; i++) {
+        if (arr[i] > arr[i + 1]) {
+            swapPositions(arr, i);
+        }
+    }
+
+    bubble_sort(arr, n - 1);
+}
+
+function swapPositions(arr: number[], index: number): void {
+    const tempNum = arr[index];
+    arr[index] = arr[index + 1];
+    arr[index + 1] = tempNum;
 }
