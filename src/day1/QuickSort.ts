0ae6fc2130147ffb0ffa246de2fd47b0e2c30b76
Kadeem Jackson <kdjackson35@gmail.com>
2024-09-18 19:19:39 -0400

    Add QuickSort solution

--- src/day1/QuickSort.ts
+++ src/day1/QuickSort.ts
@@ -1,3 +1,35 @@
-export default function quick_sort(arr: number[]): void {
+function sort(arr: number[], low: number, high: number): void {
+    if (low >= high) {
+        return;
+    }
 
+    const pivotIdx = partition(arr, low, high);
+
+    sort(arr, low, pivotIdx - 1);
+    sort(arr, pivotIdx + 1, high);
+}
+
+function partition(arr: number[], low: number, high: number): number {
+    const pivot = arr[high];
+    let idx = low - 1;
+
+    for (let i = low; i < high; i++) {
+        if (arr[i] <= pivot) {
+            idx++;
+
+            const tmp = arr[i];
+            arr[i] = arr[idx];
+            arr[idx] = tmp;
+        }
+    }
+
+    idx++;
+    arr[high] = arr[idx];
+    arr[idx] = pivot;
+
+    return idx;
+}
+
+export default function quick_sort(arr: number[]): void {
+    sort(arr, 0, arr.length - 1);
 }
